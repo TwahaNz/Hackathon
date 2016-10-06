5f993fd902484114663952643299c2fe35b9d3d8
Luke <lukekram@gmail.com>
2016-10-06 10:16:22 +0200

    added data.sql and schema.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Feature(ID, Name, URL, Votes)
+VALUES (1, 'Tom','www.google.com',2);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Feature
+(
+ ID INT NOT NULL AUTO_INCREMENT,
+ Name varchar(255),
+ URL varchar(255),
+ Votes INT,
+ PRIMARY KEY (ID)
+);
