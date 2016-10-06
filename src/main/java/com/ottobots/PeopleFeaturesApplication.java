bcd06cbdac57c83d48b0545cbc99240be4c27c63
Luke <lukekram@gmail.com>
2016-10-06 09:23:06 +0200

    first commit

---
+++ src/main/java/com/ottobots/PeopleFeaturesApplication.java
@@ -0,0 +1,12 @@
+package com.ottobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleFeaturesApplication.class, args);
+	}
+}
